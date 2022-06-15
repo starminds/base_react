f22b51119a83dc55539ac1ecb7cadf12a91cf884
starmind <dkload1588@gmail.com>
2022-06-15 09:58:15 +0900

    food

--- src/App.js
+++ src/App.js
@@ -19,7 +19,18 @@ const App = () => {
       <FnComponent_2 /> */}
 
       {/* <ClassComponent /> */}
-      <FoodMenu />
+
+      {/* *props
+      =><컴포넌트 크록스명="값"/> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" sideMenu="물" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="??" sideMenu="커피" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="???" sideMenu="홍차" />
+      {/* 2일차 */}
     </div>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,13 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴는 텐동</h3>;
+import "../../styles/style.css";
+
+export const FoodMenu = ({ menuName, sideMenu }) => {
+  // console.log(menuName);
+  // =>프록스는 오브젝트형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할것
+  return (
+    <div>
+      오늘의 메뉴는 <i className="menu">{menuName}</i> 사이드 메뉴는{" "}
+      <i className="side">{sideMenu}</i>
+      {/* jsx안쪽에 변수를 사용할시 중괄호를 감싸줌 */}
+    </div>
+  );
 };
